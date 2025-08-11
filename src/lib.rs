4270e0b8878ff050255c658bd2e95be63b2cbfd0
petscheit <etscheitp@gmail.com>
2025-08-11 17:07:44 +0200

    feat: add serde decoders for types

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 pub mod cairo_type;
 pub mod default_hints;
-pub mod stwo_utils;
 pub mod types;
 pub mod vm;
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -34,3 +34,72 @@ pub fn hex_bytes_padded(input: &str, target_len: Option<usize>) -> Result<Vec<u8
     }
     Ok(bytes)
 }
+
+#[cfg(feature = "serde")]
+pub mod serde_utils {
+    //! Serde helpers for deserializing types that implement `FromAnyStr`.
+
+    use super::FromAnyStr;
+    use serde::de::{self, Deserializer, Visitor};
+    use serde::Deserialize;
+    use std::fmt;
+
+    /// Deserialize any type implementing FromAnyStr from a JSON string
+    pub fn deserialize_from_string<'de, D, T>(deserializer: D) -> Result<T, D::Error>
+    where
+        D: Deserializer<'de>,
+        T: FromAnyStr,
+    {
+        let s = String::deserialize(deserializer)?;
+        T::from_any_str(&s).map_err(de::Error::custom)
+    }
+
+    /// Deserialize a vector of types implementing FromAnyStr from JSON string array
+    pub fn deserialize_vec_from_string<'de, D, T>(deserializer: D) -> Result<Vec<T>, D::Error>
+    where
+        D: Deserializer<'de>,
+        T: FromAnyStr,
+    {
+        let ss: Vec<String> = Vec::deserialize(deserializer)?;
+        ss.into_iter()
+            .map(|s| T::from_any_str(&s))
+            .collect::<Result<Vec<T>, _>>()
+            .map_err(de::Error::custom)
+    }
+
+    struct AnyStrVisitor<T>(std::marker::PhantomData<T>);
+
+    impl<'de, T> Visitor<'de> for AnyStrVisitor<T>
+    where
+        T: FromAnyStr,
+    {
+        type Value = T;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("a string or an integer")
+        }
+
+        fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            T::from_any_str(value).map_err(de::Error::custom)
+        }
+
+        fn visit_u64<E>(self, value: u64) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            T::from_any_str(&value.to_string()).map_err(de::Error::custom)
+        }
+    }
+
+    /// Deserialize any type implementing FromAnyStr from either a JSON string or number
+    pub fn deserialize_from_any<'de, D, T>(deserializer: D) -> Result<T, D::Error>
+    where
+        D: Deserializer<'de>,
+        T: FromAnyStr,
+    {
+        deserializer.deserialize_any(AnyStrVisitor(std::marker::PhantomData))
+    }
+}
