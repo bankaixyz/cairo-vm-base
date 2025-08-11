56f800679e2ac4c4639774bbe55448ba118ffc46
petscheit <etscheitp@gmail.com>
2025-08-11 15:27:47 +0200

    feat: add stwo FileWriter

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod cairo_type;
 pub mod default_hints;
 pub mod types;
 pub mod vm;
+pub mod stwo_utils;
---
+++ src/stwo_utils.rs
@@ -0,0 +1,36 @@
+use std::{io::Write, path::Path};
+use std::io;
+use bincode::enc::write::Writer;
+
+pub struct FileWriter {
+    buf_writer: io::BufWriter<std::fs::File>,
+    bytes_written: usize,
+}
+
+impl Writer for FileWriter {
+    fn write(&mut self, bytes: &[u8]) -> Result<(), bincode::error::EncodeError> {
+        self.buf_writer
+            .write_all(bytes)
+            .map_err(|e| bincode::error::EncodeError::Io {
+                inner: e,
+                index: self.bytes_written,
+            })?;
+
+        self.bytes_written += bytes.len();
+
+        Ok(())
+    }
+}
+
+impl FileWriter {
+    fn new(buf_writer: io::BufWriter<std::fs::File>) -> Self {
+        Self {
+            buf_writer,
+            bytes_written: 0,
+        }
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.buf_writer.flush()
+    }
+}
