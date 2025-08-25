8d844bfa830045ac7920bf52218a0bc8206353a8
petscheit <etscheitp@gmail.com>
2025-08-25 15:26:57 +0200

    feat: move decodeable traits to base type to simplify trait impl

--- src/cairo_type.rs
+++ src/cairo_type.rs
@@ -4,12 +4,12 @@ use cairo_vm::{
     vm::{errors::hint_errors::HintError, vm_core::VirtualMachine},
 };
 
-pub trait BaseCairoType {
+pub trait BaseCairoType: FromAnyStr + Sized + CairoType {
     fn from_bytes_be(bytes: &[u8]) -> Self;
     fn bytes_len() -> usize;
 }
 
-pub trait CairoType: Sized + FromAnyStr + BaseCairoType {
+pub trait CairoType: Sized {
     fn from_memory(vm: &VirtualMachine, address: Relocatable) -> Result<Self, HintError>;
     fn to_memory(
         &self,
